76143c6ce5c5720605bc03fce33656a805ff457c
Ujjawal Gusain <ujjawalgusain31@gmail.com>
2026-04-16 12:33:25 +0530

    Update base url for http secure url

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,8 +1,7 @@
-const BASE_URL = "http://184.72.74.143:8000"
+const BASE_URL = "https://quakr-store.com"
 
 export const APIS = {
     CREATE_TASK: `${BASE_URL}/task/create`,
     EXECUTE_STEP: `${BASE_URL}/task/execute-step`,
     TRANSCRIBE: `${BASE_URL}/task/transcribe`,
-
 }
